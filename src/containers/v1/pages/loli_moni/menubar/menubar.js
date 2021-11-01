de9f68cfd91021126cd8eed5300d34ac439d2d56
Yuven99 <yuvenraj@fleetata.com>
2021-11-01 13:23:11 +0800

    Update layout again

--- src/containers/v1/pages/loli_moni/menubar/menubar.js
+++ src/containers/v1/pages/loli_moni/menubar/menubar.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { Layout, Menu, PageHeader } from 'antd';
+import './menubar.css';
+
+import { Layout, Menu, PageHeader, Breadcrumb } from 'antd';
 import {
   MenuUnfoldOutlined,
   MenuFoldOutlined,
   UserOutlined,
   VideoCameraOutlined,
   UploadOutlined,
+  LaptopOutlined,
+  NotificationOutlined
 } from '@ant-design/icons';
 
 import TableShow from '../table/table'
 import ModalShow from '../modal/modal';
 
+const { SubMenu } = Menu;
 const { Header, Sider, Content } = Layout;
 
 class SiderDemo extends React.Component {
@@ -27,48 +32,70 @@ class SiderDemo extends React.Component {
   render() {
     return (
       <Layout>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" icon={<UserOutlined />}>
-              nav 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-              nav 2
-            </Menu.Item>
-            <Menu.Item key="3" icon={<UploadOutlined />}>
-              nav 3
-            </Menu.Item>
-          </Menu>
-        </Sider>
-
-        <Layout>
-            <Header style={{ padding: 0, backgroundColor: "white" }}>
-                <div style = {{ marginLeft: 5 }}> 
-                    <MenuFoldOutlined
-                        onClick = {() => {
-                            this.toggle();
-                        }}
-                        style = {{ fontSize: 20 }}
-                    />
-                </div>
-            </Header>
-
-            <Content
-                className="site-layout-background"
-                style={{
-                    margin: '24px 16px',
-                    padding: 24,
-                    minHeight: 280,
-                }}
-            >
-                Content
-
-                <ModalShow/>
-                <TableShow/>
-            </Content>
-        </Layout>
+        <div style={{textAlign:'center', fontSize:30}}>
+          <h1>LoLi Monitoring</h1>
+        </div>
+    <Header className="header">
+      <div style={{float: 'left',
+    width: 120,
+    height: 31,
+    margin: '16px 24px 16px 0',
+    background: 'red',}} />
+      <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Layout>
+      <Sider style={{ width:'auto', height:600}}>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          style={{ height: '100%', borderRight: 0 }}
+        >
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+            <Menu.Item key="5">option5</Menu.Item>
+            <Menu.Item key="6">option6</Menu.Item>
+            <Menu.Item key="7">option7</Menu.Item>
+            <Menu.Item key="8">option8</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+            <Menu.Item key="9">option9</Menu.Item>
+            <Menu.Item key="10">option10</Menu.Item>
+            <Menu.Item key="11">option11</Menu.Item>
+            <Menu.Item key="12">option12</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item >Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Content
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+            background: 'white',
+          }}
+        >
+          Content
+          <ModalShow/>
+          <TableShow/>
+        </Content>
       </Layout>
+    </Layout>
+  </Layout>
     );
   }
 }
